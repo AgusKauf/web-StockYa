3a32a90a629ba02bae05917c461523c5d8bd969d
Agustin Kaufman <aguskauf123@gmail.com>
2022-06-13 23:18:56 -0300

    include array

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 // Simulador interactivo
 
+// Array y base de datos
+
+class Productos{
+    constructor(id, nombre, precio, stock){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.stock = stock
+    }
+}
+
+const producto = [
+    new Productos(1, "Diamante", 1000, 300),
+    new Productos(2, "Tamarindo", 5000, 100),
+    new Productos(3, "Nordika", 5000, 200),
+    new Productos(4, "Black Out", 8000, 100),
+]
+
+function agregarProducto(){
+    let id = Number(prompt("Ingrese el ID de producto:"))
+    let nombre = prompt("Ingrese el nombre del producto:")
+    let precio = Number(prompt("Ingrese el precio del producto:"))
+    let stock = Number(prompt("Ingrese el stock del producto:"))
+
+    producto.push(new Productos(id, nombre, precio, stock))
+
+}
+
+agregarProducto()
+
+console.log(producto)
+
+
+// Login + opción de compra
+
 let user = prompt("Ingrese su usuario:")
 let password = prompt("Ingrese su contraseña:")
 
@@ -17,7 +52,7 @@ if( (user === "AgustinK") && (password === "Agus1234") ) {
         switch (compra.toLowerCase()){
 
             case "porcelanatos":
-                precio = 1500
+                precio = 5000
                 alert("El valor del porcelanato es: $" + precio)
                 break;
             
